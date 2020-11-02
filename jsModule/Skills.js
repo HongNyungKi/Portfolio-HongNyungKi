be40c874963284e88258de6b7f5dc30dc8a40097
HongNyungKi <hnk2017@naver.com>
2020-11-02 17:43:11 +0900

    add

--- jsModule/Skills.js
+++ jsModule/Skills.js
@@ -1,5 +1,64 @@
+const SKILLS_ITEMS = [
+    {
+      name: 'HTML',
+      score: 80
+    },
+    {
+      name: 'CSS',
+      score: 80
+    },
+    {
+      name: 'JAVASCRIPT',
+      score: 80
+    },
+    {
+      name: 'SCSS',
+      score: 70
+    },
+    {
+      name: 'JQUERY',
+      score: 60
+    },
+    {
+      name: 'REACT',
+      score: 50
+    }
+  ];
+
 export default class Skills{
     constructor(){
+        this.target = document.querySelector('.skillset__left');
+    }
+    
+    loadSkills(){
+        SKILLS_ITEMS.forEach(item => {
+            const containerDiv = document.createElement('div');
+            const descriptionDiv = document.createElement('div');
+            const nameEl = document.createElement('span');
+            const scoreEl = document.createElement('span');
+            const skillBarContainer = document.createElement('div');
+            const skillBar = document.createElement('div');
         
+            this.target.appendChild(containerDiv);
+            containerDiv.setAttribute('class', 'skill');
+            containerDiv.appendChild(descriptionDiv);
+            descriptionDiv.setAttribute('class', 'skill__description');
+            descriptionDiv.appendChild(nameEl);
+            descriptionDiv.appendChild(scoreEl);
+            
+            nameEl.setAttribute('class', 'des__name');
+            scoreEl.setAttribute('class', 'des__score');
+        
+            nameEl.innerText = item.name;
+            scoreEl.innerText = `${item.score}%`;
+        
+            containerDiv.appendChild(skillBarContainer);
+            skillBarContainer.setAttribute('class', 'skill__bar');
+            skillBarContainer.appendChild(skillBar);
+        
+            skillBar.setAttribute('class', 'bar__value');
+            skillBar.setAttribute('style', `width: ${item.score}%`);
+        
+          })
     }
 }
--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 import Navbar from './jsModule/Navbar.js';
 import Home from './jsModule/Home.js';
+import Skills from './jsModule/Skills.js';
 import Work from './jsModule/Work.js';
 import ArrowUp from './jsModule/ArrowUp.js';
 
 const navbar = new Navbar();
 const home = new Home();
+const skills = new Skills();
 const work = new Work();
 const arrowUp = new ArrowUp();
 
@@ -16,6 +18,7 @@ navbar.clickAutoScroll();
 navbar.respondMenu();
 home.contactAutoScroll();
 home.homeOpacityDown();
+skills.loadSkills();
 work.controllWorkProject();
 arrowUp.showArrowBtn();
 arrowUp.arrowAutoScroll();
